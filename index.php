74ed35c4b859196dd0f1380b26cfab411c0f975f
Missael_Reyes <L16181173@slp.tecnm.mx>
2020-12-02 17:22:36 -0600

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+	<h1>Diccionario del Español a Ingles(Version 1)</h1>
+	<h2>Servicio web + PHP + SOAP</h2>
+	<form action ="resultado.php" method="GET">
+		<input type='text' name='palabra'>
+		<input type='submit' name='enviar' value='Traducir'>
+	</form>	
+</body>
+</html>
